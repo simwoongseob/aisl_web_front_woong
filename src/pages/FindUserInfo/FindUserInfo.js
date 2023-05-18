8dea6f5990eeb654c1f833a9575d6f66f6e008c1
simwoongseob <simwoongseob@gmail.com>
2023-05-18 18:02:38 +0900

    woong_first commit

--- src/pages/FindUserInfo/FindUserInfo.js
+++ src/pages/FindUserInfo/FindUserInfo.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Row, Col } from "antd";
+import styles from "./findUserInfo.module.css";
 
 import PageTitle from "../../components/PageTitle/PageTitle";
+import FindUserInfoEmail from "./ForgotEmail";
+import FindUserInfoPassword from "./ForgotPassword";
 
 const FindUserInfo = () => {
   return (
     <div style={{ marginBottom: 100 }}>
       <PageTitle title="FindUserInfo" />
+      <h2 className={styles.page_date}>2023-05-13</h2>
+
+      <Row style={{ marginLeft: 50, marginRight: 50 }}>
+        {/* 왼쪽 - 이메일 찾기 */}
+        <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+          <FindUserInfoEmail />
+        </Col>
+
+        {/* 왼쪽-오른쪽 사이 중간 여백 */}
+        <Col span={2}></Col>
+
+        {/* 오른쪽 섹션1 - 주요일정 */}
+        <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+          <FindUserInfoPassword />
+        </Col>
+      </Row>
     </div>
   );
 };
---
+++ src/pages/FindUserInfo/ForgotEmail.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Button, Form, Input, ConfigProvider, Space, Select } from "antd";
+import { LockOutlined, UserOutlined, MailOutlined } from "@ant-design/icons";
+
+import PageTitle from "../../components/PageTitle/PageTitle";
+
+import styles from "./findUserInfo.module.css";
+import { colors } from "../../assets/colors";
+import { question } from "../../assets/string/question";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+const Forgotemail = () => {
+  return (
+    <div>
+      <PageTitle title="Forgot Email" />
+      <div className={styles.form_container}>
+        <Form
+          name="basic"
+          colon={false}
+          style={{
+            minWidth: 500,
+            maxWidth: 600,
+          }}
+          // initialValues={{
+          //   remember: true,
+          // }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          layout="vertical"
+        >
+          <Form.Item
+            label={<span className={styles.label}>Name</span>}
+            name="Name"
+            rules={[
+              {
+                // required: true,
+                message: "이름을 적어주세요",
+              },
+            ]}
+          >
+            <Input
+              prefix={<MailOutlined style={{ marginRight: 5 }} />}
+              placeholder="이름을 적어주세요"
+              size="large"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label={
+              <span className={styles.label}>
+                Identity verification question
+              </span>
+            }
+            name="question"
+          >
+            <Select
+              defaultValue="가장 인상깊게 읽은 책은?"
+              options={question}
+              size="large"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label={<span className={styles.label}>Answer</span>}
+            name="answer"
+          >
+            <Input
+              required={true}
+              placeholder="질문 답변을 적어주세요"
+              size="large"
+            />
+          </Form.Item>
+
+          {/* <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item> */}
+          <br />
+          <br />
+          <br />
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: colors.yiu_dark_blue_light,
+                colorPrimaryHover: colors.yiu_dark_blue,
+              },
+            }}
+          >
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                block={true}
+                size="large"
+                style={{ height: 50 }}
+              >
+                이메일 찾기
+              </Button>
+            </Form.Item>
+          </ConfigProvider>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Forgotemail;
